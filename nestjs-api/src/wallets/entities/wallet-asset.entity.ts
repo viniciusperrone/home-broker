4a0bc6f0719a6e02c44520608a1bf68c1c9ebb15
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-23 10:47:28 -0300

    feat: created wallet-asset entity

---
+++ nestjs-api/src/wallets/entities/wallet-asset.entity.ts
@@ -0,0 +1,33 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import crypto from "crypto";
+
+import { Wallet, WalletDocument } from "./wallet.entity";
+import { Asset, AssetDocument } from "src/assets/entities/asset.entity";
+
+export type WalletAssetDocument = HydratedDocument<WalletAsset>;
+
+@Schema({ timestamps: true })
+export class WalletAsset {
+  @Prop({ default: () => crypto.randomUUID() })
+  _id: string;
+
+  @Prop({ type: mongoose.Schema.Types.Int32 })
+  shares: number;
+
+  @Prop({ type: String, ref: Wallet.name })
+  wallet: WalletDocument | string;
+
+  @Prop({ type: String, ref: Asset.name })
+  asset: AssetDocument | string;
+
+  @Prop()
+  createdAt!: Date;
+
+  @Prop()
+  updatedAt!: Date;
+}
+
+export const WalletAssetSchema = SchemaFactory.createForClass(WalletAsset);
+
+WalletAssetSchema.index({ wallet: 1, asset: 1 }, { unique: true })
--- nestjs-api/src/wallets/wallets.module.ts
+++ nestjs-api/src/wallets/wallets.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 import { Wallet, WalletSchema } from './entities/wallet.entity';
+import { WalletAsset, WalletAssetSchema } from './entities/wallet-asset.entity';
 
 @Module({
   imports: [
@@ -11,6 +12,10 @@ import { Wallet, WalletSchema } from './entities/wallet.entity';
       {
         name: Wallet.name,
         schema: WalletSchema
+      },
+      {
+        name: WalletAsset.name,
+        schema: WalletAssetSchema
       }
     ])
   ],
