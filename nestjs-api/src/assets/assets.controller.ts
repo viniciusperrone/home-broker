fb7c69f01e1dad27d31cdee77502630611648b58
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-23 02:21:40 -0300

    feat: assets module

--- nestjs-api/src/assets/assets.controller.ts
+++ nestjs-api/src/assets/assets.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
-import { UpdateAssetDto } from './dto/update-asset.dto';
 
 @Controller('assets')
 export class AssetsController {
@@ -17,18 +16,9 @@ export class AssetsController {
     return this.assetsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.assetsService.findOne(+id);
+  @Get(':symbol')
+  findOne(@Param('symbol') symbol: string) {
+    return this.assetsService.findOne(symbol);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAssetDto: UpdateAssetDto) {
-    return this.assetsService.update(+id, updateAssetDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.assetsService.remove(+id);
-  }
 }
--- nestjs-api/src/assets/dto/create-asset.dto.ts
+++ nestjs-api/src/assets/dto/create-asset.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAssetDto {}
+export class CreateAssetDto {
+  name: string;
+
+  symbol: string;
+
+  price: string;
+  
+  image: string;
+}
